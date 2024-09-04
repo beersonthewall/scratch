aef22777a9117ab5d5349cbcfdb4a1db2945fe2d
Matthew <matthew@beersonthewall.com>
2024-09-04 10:47:02 -0500

    learning_c: check argument count and print usage.

--- learning_c/calculations/main.c
+++ learning_c/calculations/main.c
@@ -1,5 +1,19 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-  printf("Hello, World!\n");
+const char* usage = "Usage:\n calc {sum, product, mean} [nums]\nExample:\n calc sum [1,2,3]\n";
+
+int main(int argc, char* argv[]) {
+  if(argc != 3) {
+    fprintf(stderr, "%s", usage);
+    exit(EXIT_FAILURE);
+  }
+
+  char* fst = argv[1];
+  //char* snd = argv[2];
+
+  if(strcmp(fst, "sum") == 0) {
+    printf("sum\n");
+  }
 }
